1a1469ac2228ea3bc57f2c27f58ed698ec2f5b7e
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-02-17 13:51:48 +0100

    Done

---
+++ queriesTienda.sql
@@ -0,0 +1,92 @@
+use tienda;
+/*1*/
+select nombre from producto;
+/*2*/
+select nombre, precio from producto;
+/*3*/
+select * from producto;
+/*4*/
+select nombre, precio, ROUND(precio * 1.05, 2) as precio_dollar from producto;
+/*5*/
+select nombre as nom_de_producte, precio as euros, ROUND(precio * 1.05, 2) as dollars from producto;
+/*6*/
+select upper(nombre), precio from producto;
+/*7*/
+select lower(nombre), precio from producto;
+/*8*/
+select nombre, upper(substr(nombre,1,2)) from fabricante;
+/*9*/
+select nombre, round(precio) from producto;
+/*10*/
+select nombre, truncate(precio,0) from producto;
+/*11*/
+select fabricante.codigo from fabricante inner join producto on fabricante.codigo = codigo_fabricante;
+/*12*/
+select fabricante.codigo from fabricante inner join producto on fabricante.codigo = codigo_fabricante group by fabricante.codigo;
+/*13*/
+select nombre from fabricante order by nombre asc;
+/*14*/
+select nombre from fabricante order by nombre desc;
+/*15*/
+select nombre, precio from producto order by nombre asc, precio desc;
+/*16*/
+select * from fabricante limit 5;
+/*17*/
+select * from fabricante limit 3,2;
+/*18*/
+select nombre, precio from producto order by precio asc limit 1;
+/*19*/
+select nombre, precio from producto order by precio desc limit 1;
+/*20*/
+select producto.nombre from producto right join fabricante on producto.codigo_fabricante = fabricante.codigo where fabricante.codigo = 2;
+/*21*/
+select producto.nombre, precio, fabricante.nombre from producto left join fabricante on producto.codigo_fabricante = fabricante.codigo;
+/*22*/
+select producto.nombre, precio, fabricante.nombre from producto left join fabricante on producto.codigo_fabricante = fabricante.codigo order by fabricante.nombre asc;
+/*23*/
+select producto.codigo, producto.nombre, producto.codigo, fabricante.nombre from producto left join fabricante on producto.codigo_fabricante = fabricante.codigo;
+/*24*/
+select producto.nombre, precio, fabricante.nombre from producto left join fabricante on producto.codigo_fabricante = fabricante.codigo order by precio asc limit 1;
+/*25*/
+select producto.nombre, precio, fabricante.nombre from producto left join fabricante on producto.codigo_fabricante = fabricante.codigo order by precio desc limit 1;
+/*26*/
+select producto.nombre from producto left join fabricante on producto.codigo_fabricante = fabricante.codigo where fabricante.nombre = 'Lenovo';
+/*27*/
+select producto.nombre from producto left join fabricante on producto.codigo_fabricante = fabricante.codigo where fabricante.nombre = 'Crucial' and precio >200;
+/*28*/
+select producto.nombre from producto left join fabricante on producto.codigo_fabricante = fabricante.codigo where fabricante.nombre = 'Asus' or fabricante.nombre = 'Hewlett-Packard' or fabricante.nombre = 'Seagate';
+/*29*/
+select producto.nombre from producto left join fabricante on producto.codigo_fabricante = fabricante.codigo where fabricante.nombre IN('Asus', 'Hewlett-Packard','Seagate');
+/*30*/
+select producto.nombre, precio from producto left join fabricante on producto.codigo_fabricante = fabricante.codigo where substring(fabricante.nombre, -1) = 'e';
+/*31*/
+select producto.nombre, precio from producto left join fabricante on producto.codigo_fabricante = fabricante.codigo where locate('w',fabricante.nombre) > 0;
+/*32*/
+select producto.nombre, precio, fabricante.nombre from producto left join fabricante on producto.codigo_fabricante = fabricante.codigo where precio >=180 order by precio desc, producto.nombre asc;
+/*33*/
+select fabricante.codigo, fabricante.nombre from producto left join fabricante on producto.codigo_fabricante = fabricante.codigo group by fabricante.nombre, fabricante.codigo;
+/*34*/
+select fabricante.codigo, fabricante.nombre, producto.nombre from producto right join fabricante on producto.codigo_fabricante = fabricante.codigo;
+/*35*/
+select fabricante.codigo, fabricante.nombre from producto right join fabricante on producto.codigo_fabricante = fabricante.codigo where producto.codigo is null;
+/*36*/
+select producto.codigo, producto.nombre from producto left join fabricante on producto.codigo_fabricante = fabricante.codigo where fabricante.nombre = 'Lenovo';
+/*37*/
+select * from producto where precio = (select max(precio) from producto left join fabricante on producto.codigo_fabricante = fabricante.codigo where fabricante.nombre = 'Lenovo');
+/*38*/
+select producto.nombre from producto inner join fabricante on producto.codigo_fabricante = fabricante.codigo where fabricante.nombre = 'Lenovo' and precio = ( select max(precio) from producto inner join fabricante on producto.codigo_fabricante = fabricante.codigo where fabricante.nombre = 'Lenovo') limit 1;
+/*39*/
+select producto.nombre from producto inner join fabricante on producto.codigo_fabricante = fabricante.codigo where fabricante.nombre = 'Hewlett-Packard' and precio = ( select min(precio) from producto inner join fabricante on producto.codigo_fabricante = fabricante.codigo where fabricante.nombre = 'Hewlett-Packard' ) limit 1;
+/*40*/
+select producto.nombre from producto inner join fabricante on producto.codigo_fabricante = fabricante.codigo where precio >= (select precio from producto inner join fabricante on producto.codigo_fabricante = fabricante.codigo where fabricante.nombre = 'Lenovo' and precio = ( select max(precio) from producto inner join fabricante on producto.codigo_fabricante = fabricante.codigo where fabricante.nombre = 'Lenovo') limit 1);
+/*41*/
+select producto.codigo, producto.nombre from producto  inner join fabricante on fabricante.codigo = producto.codigo_fabricante where fabricante.nombre = 'Asus' and precio > (select sum(producto.precio)/count(producto.codigo) from producto inner join fabricante on fabricante.codigo = producto.codigo_fabricante where fabricante.nombre = 'Asus');
+
+
+
+
+
+
+
+
+
---
+++ queriesUniversity.sql
@@ -0,0 +1,61 @@
+USE universidad;
+/*Primer grupo*/
+/*1*/
+select apellido1, apellido2,nombre from persona where tipo = 'alumno' order by apellido1, apellido2, nombre asc; 
+/*2*/
+select apellido1, apellido2,nombre from persona where tipo = 'alumno' and telefono is null order by apellido1, apellido2, nombre asc; 
+/*3*/
+select apellido1, apellido2,nombre from persona where tipo = 'alumno' and YEAR(fecha_nacimiento) = 1999 order by apellido1, apellido2, nombre asc; 
+/*4*/
+select apellido1, apellido2,nombre,nif from persona where tipo = 'profesor' and telefono is null and nif regexp '[0-9]*K' order by apellido1, apellido2, nombre asc; 
+/*5*/
+select * from asignatura where cuatrimestre = 1 and curso = 3 and id_grado = 7;
+/*6*/
+select apellido1,apellido2, persona.nombre,departamento.nombre as Departamento from persona inner join profesor on persona.id = id_profesor inner join departamento on departamento.id = profesor.id_departamento order by apellido1, apellido2, persona.nombre;
+/*7*/
+select apellido1,apellido2, persona.nombre,departamento.nombre as Departamento from persona inner join profesor on persona.id = id_profesor inner join departamento on departamento.id = profesor.id_departamento order by apellido1, apellido2, persona.nombre asc;
+/*8*/
+select departamento.nombre as Departamento from profesor inner join departamento on departamento.id = profesor.id_departamentoinner join asignatura on profesor.id_profesor = asignatura.id_profesor inner join grado on asignatura.id_grado = grado.id group by departamento.nombre;
+/*9*/
+select apellido1,apellido2,nombre from persona inner join alumno_se_matricula_asignatura on persona.id = id_alumno inner join curso_escolar on id_curso_escolar= curso_escolar.id where curso_escolar.anyo_inicio = 2014 group by apellido1,apellido2,nombre;
+/*Segundo grupo*/
+/*1*/
+select departamento.nombre, apellido1,apellido2,persona.nombre from departamento right join profesor on departamento.id = profesor.id_departamento inner join persona on profesor.id_profesor = persona.id order by departamento.nombre, apellido1, apellido2, persona.nombre;
+/*2*/
+select id_profesor from profesor where profesor.id_departamento is NUll;
+/*3*/
+select departamento.nombre as NombreDepartamento from profesor right join departamento on departamento.id = profesor.id_departamento where id_profesor is null;
+/*4*/
+select persona.id, persona.nombre, apellido1, apellido2 from asignatura right join profesor on asignatura.id_profesor = profesor.id_profesor right join persona on persona.id = profesor.id_profesor where asignatura.id is null and persona.tipo = 'profesor';
+/*5*/
+select nombre from asignatura where asignatura.id_profesor is null;
+/*6*/
+select departamento.nombre from asignatura right join profesor on asignatura.id_profesor = profesor.id_profesor right join departamento  on departamento.id = profesor.id_departamento where asignatura.id is null group by departamento.nombre;
+/*Tercer grupo*/
+/*1*/
+select count(id) as NumAlumnos from persona where tipo = 'alumno' group by tipo;
+/*2*/
+select count(id) as NumAlumnos1999 from persona where tipo = 'alumno' and YEAR(fecha_nacimiento) = 1999 group by tipo;
+/*3*/
+select departamento.nombre, count(id_profesor) as NumProfesores  from departamento right join profesor on departamento.id = profesor.id_departamento group by departamento.id order by count(id_profesor) asc;
+/*4*/
+select departamento.nombre, persona.nombre, persona.apellido1, persona.apellido2  from profesor right join persona on profesor.id_profesor = persona.id right join departamento on departamento.id = profesor.id_departamento order by departamento.nombre asc;
+/*5*/
+select grado.nombre, count(asignatura.id) as NumAsignaturas from asignatura right join grado on asignatura.id_grado = grado.id group by grado.id order by count(asignatura.id) desc;
+/*6*/
+select grado.nombre, count(asignatura.id) as NumAsignaturas from asignatura right join grado on asignatura.id_grado = grado.id group by grado.id having count(asignatura.id) >40 order by count(asignatura.id) desc;
+/*7*/
+select grado.nombre, asignatura.tipo, sum(asignatura.creditos) from asignatura left join grado on asignatura.id_grado = grado.id group by asignatura.tipo, grado.nombre;
+/*8*/
+select anyo_inicio, count(id_alumno) from alumno_se_matricula_asignatura right join curso_escolar on id_curso_escolar= curso_escolar.id group by curso_escolar.id;
+/*9*/
+select persona.id, persona.nombre, apellido1, apellido2, count(asignatura.id) NumAsignaturas from profesor inner join persona on profesor.id_profesor = persona.id left join asignatura  on persona.id = asignatura.id_profesor group by persona.id order by count(asignatura.id) desc;
+/*10*/
+select * from persona where fecha_nacimiento = (select max(fecha_nacimiento) from persona where tipo = 'alumno');
+/*11*/
+select persona.id, persona.nombre, apellido1, apellido2, departamento.nombre as NombreDepartamento from profesor inner join departamento on departamento.id = profesor.id_departamento inner join persona on profesor.id_profesor = persona.id left join asignatura on profesor.id_profesor = asignatura.id_profesor group by profesor.id_profesor having count(asignatura.id)= 0;
+
+
+
+
+
